948feeeee3c2cb69662d466787688058a84c13fa
PIKACHUIM <pikachuim@qq.com>
2023-11-07 14:17:10 +0800

    Updated20231107141710

--- Upload.bat
+++ Upload.bat
@@ -167,6 +167,7 @@ git add .
 git commit -m "Updated"%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
 git push
 git push -u remote
+git push -u origin
 timeout /t 5   >nul
 goto LABEL_SUCC
 REM ###############################################################
